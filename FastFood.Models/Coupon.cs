9c2cadc447647fc77e02dc680866bee6d26037ae
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-09 14:32:39 +0700

    update 2

---
+++ FastFood.Models/Coupon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class Coupon
+    {
+        public int If { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public double Discount { get; set; }
+        public double Minimumamount { get; set; }
+        public byte[] CouponPicture { get; set; }
+        public bool IsActives { get; set; }
+    }
+    public enum couponType
+    {
+        Precent=0,
+        Currency=1
+    }
+}

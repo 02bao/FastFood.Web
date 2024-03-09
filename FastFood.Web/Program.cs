e1ecfb96fe833aff338246b0123a3083379e828d
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-09 15:23:51 +0700

    update 3 1p

--- FastFood.Web/Program.cs
+++ FastFood.Web/Program.cs
@@ -1,4 +1,7 @@
 using FastFood.Repository;
+using FastFood.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace FastFood.Web
@@ -13,6 +16,12 @@ namespace FastFood.Web
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext < ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefauleConnections")));
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                            .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
